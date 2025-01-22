8fb4781b53f9d79938da74180c033d7b0ce4d8b7
Ayoub <ayoubsolicode07@gmail.com>
2025-01-22 09:13:58 +0000

    add examples

---
+++ blade_veille/app/Http/Controllers/home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class home extends Controller
+{
+    //
+}
+class MetricsService {
+    public function MonthlyRevenue(){
+        return '10,000$';
+    }
+}
